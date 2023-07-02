14b608596db6b03f5639bf7421d27ffe8bb61937
David Forrester <david.o.forrester@gmail.com>
2023-07-02 18:27:38 +1000

    appending and printing the list

---
+++ linked-list.js
@@ -0,0 +1,39 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  print() {
+    let node = this.head;
+    while (node) {
+      console.log(node.value);
+      node = node.next;
+    }
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+
+const linkedList = new LinkedList();
+linkedList.append("testing");
+linkedList.append("testing a second one");
+
+linkedList.print();
